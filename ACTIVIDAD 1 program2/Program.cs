3b14b163b92b4bcb0c19b1594be1a4e631606e16
michelle111801 <82916706+michelle111801@users.noreply.github.com>
2021-04-22 23:54:04 -0400

    Add files via upload

---
+++ ACTIVIDAD 1 program2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ACTIVIDAD_1_program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Progr2!");
+        }
+    }
+}
